4b30bcf1ac0e81e26e3afb682deedd2f515fbf77
dothecoolwip1 <160705346+dothecoolwip1@users.noreply.github.com>
2026-09-11 20:31:58 -0600

    Wire auth enhancements into app

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import AuthEnhancements from './AuthEnhancements'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <AuthEnhancements />
     </BrowserRouter>
   </React.StrictMode>,
 )

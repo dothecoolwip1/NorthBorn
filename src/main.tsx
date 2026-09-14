b530a1bb8b837d03c6c341bcb0ca4add4ce09efd
dothecoolwip1 <160705346+dothecoolwip1@users.noreply.github.com>
2026-09-14 10:28:36 -0600

    Wire team access and invite routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import AuthEnhancements from './AuthEnhancements'
+import TeamAccessPage, { JoinOrganizationPage, TeamAccessLauncher } from './TeamAccess'
 import './styles.css'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      <AuthEnhancements />
+      {path === '/team-access' ? (
+        <TeamAccessPage />
+      ) : path === '/join' ? (
+        <JoinOrganizationPage />
+      ) : (
+        <>
+          <App />
+          <AuthEnhancements />
+          <TeamAccessLauncher />
+        </>
+      )}
     </BrowserRouter>
   </React.StrictMode>,
 )

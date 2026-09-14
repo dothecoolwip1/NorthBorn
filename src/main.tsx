7afa82d0f95550236bdae45a10745d379a3d4b8f
dothecoolwip1 <160705346+dothecoolwip1@users.noreply.github.com>
2026-09-14 10:37:42 -0600

    Use fixed Team Access page loader

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import AuthEnhancements from './AuthEnhancements'
-import TeamAccessPage, { JoinOrganizationPage, TeamAccessLauncher } from './TeamAccess'
+import { JoinOrganizationPage, TeamAccessLauncher } from './TeamAccess'
+import TeamAccessPage from './TeamAccessPage'
 import './styles.css'
 
 const path = window.location.pathname

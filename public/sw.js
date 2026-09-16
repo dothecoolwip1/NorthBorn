6baa6daff94c64d2bf09260a7d6e1c59db264132
dothecoolwip1 <160705346+dothecoolwip1@users.noreply.github.com>
2026-09-15 22:38:59 -0600

    Refresh PWA cache for 0.2.1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'northborn-shell-v0.2.0'
+const CACHE_NAME = 'northborn-shell-v0.2.1'
 const CORE_ASSETS = ['/', '/manifest.webmanifest', '/icons/northborn-icon.svg']
 
 self.addEventListener('install', event => {
